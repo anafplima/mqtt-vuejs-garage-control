9ccc7582383159205adad0fcb8e46d385ddd55da
Ana Flavia Lima <anaflaviapedrolima@gmail.com>
2019-11-27 19:52:49 -0300

    Adicionando alteraçoes no html

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>Ana Garagem</h1>
+      <h1>Garage</h1>
     </div>
     <div class="container">
-      <h2>Controle</h2>
+      <h2>Control</h2>
     </div>
     <div class="container">
       <div class="row">
@@ -18,7 +18,7 @@
     </div>
     <div class="container">
       <p>
-        Garagem:
+        Garage:
         <span v-html="connected"></span>
       </p>
     </div>
@@ -28,6 +28,15 @@
         <span v-html="message"></span>
       </p>
     </div>
+    <footer>
+      <nav class="navbar navbar-dark bg-dark justify-content-center">
+        <div class="row">
+          <div class="col-12 text-center text-white">
+            <p>&copy; Copyright- 2019 | Ana & Glenda</p>
+          </div>
+        </div>
+      </nav>
+    </footer>
   </div>
 </template>
 
@@ -73,6 +82,10 @@ export default {
       this.$mqtt.publish("garage/open", "true");
     },
     closeGarageDoor() {
+      if (!this.connected) {
+        return alert("A garagem está desconectada.");
+      }
+
       if (this.garageState == "close") {
         return alert("A garagem já está fechada.");
       }
